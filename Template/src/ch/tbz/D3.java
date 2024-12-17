9d1e6efbbee4ea132752fd57c9cf9da168729d45
ericluec <eric.luecking@gmail.com>
2024-12-17 15:36:51 +0100

    excercise and leraningjournal added

---
+++ Template/src/ch/tbz/D3.java
@@ -0,0 +1,14 @@
+package ch.tbz;
+
+import static ch.tbz.lib.Input.inputFloat;
+import static ch.tbz.lib.Input.inputString;
+import static java.lang.System.*;   // System-IO Library for abbriviation purpose to static functions
+import static java.lang.Math.*;     // Mathematic Library for abbriviation purpose to static functions
+import java.util.*;
+
+public class D3 {
+    public static void main(String[] args ) {
+        String satzInput = inputString("Geben sie einen Satz ein: ");
+
+    }
+}
