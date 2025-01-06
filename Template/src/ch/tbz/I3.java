ff4cee11ba6e7cd87cac36e6312ed789f803b96f
ericluec <eric.luecking@gmail.com>
2025-01-06 14:35:44 +0100

    i3 done

---
+++ Template/src/ch/tbz/I3.java
@@ -0,0 +1,68 @@
+package ch.tbz;
+
+import static ch.tbz.lib.Input.*;
+import static java.lang.System.out;
+
+public class I3 {
+    public static void main(String[] args) {
+        boolean rerunProgram;
+            do {
+                out.println("Berechnungsfunktionen:");
+                out.println("[1] Fläche");
+                out.println("[2] Masseinheiten");
+                out.println("[3] Pythagoras");
+                int functionHolder = inputInt("Wähle eine Funktion:");
+
+                if (functionHolder == 1) {
+                    out.println("Gebe die Seitenlängen in cm an:");
+                    int firstSide = inputInt("[1] Seite");
+                    int secondSide = inputInt("[2] Seite");
+                    int calculatedSpace = firstSide * secondSide;
+                    out.println("Ihre Fläche beträgt: " + calculatedSpace + "cm.");
+                } else if (functionHolder == 2) {
+                    out.println("Gib die Masseinheit an die du verrechnen Möchtest");
+                    out.println("[1] KG");
+                    out.println("[2] Pfund");
+                    out.println("[3] Tonnen");
+                    int calculatingWeight = inputInt("Zu verrechnende Gewichtseinheit");
+                    int goalWeightCategory = inputInt("Ziel-Gewichtseinheit");
+                    int weightInput = inputInt("Geben sie nun das zu verrechnende Gewicht in der entsprechenden Einheit ein");
+                    double gramIndicator = 0;
+                    if (calculatingWeight == 1) {
+                        gramIndicator = weightInput * 1000;
+                    } else if (calculatingWeight == 2) {
+                        gramIndicator = weightInput * 453.592;
+                    } else if (calculatingWeight == 3) {
+                        gramIndicator = weightInput * 1000000;
+                    }
+                    double WeightResult = 0;
+                    if (goalWeightCategory == 1) {
+                        WeightResult = gramIndicator / 1000;
+                    } else if (goalWeightCategory == 2) {
+                        WeightResult = gramIndicator / 453.592;
+                    } else if (goalWeightCategory == 3) {
+                        WeightResult = gramIndicator / 1000000;
+                    }
+                    out.println("Resultat: " + WeightResult);
+                } else if (functionHolder == 3) {
+                    out.println("Welche Seite des Dreiecks möchtest du berechnen?");
+                    out.println("[1] Kathete");
+                    out.println("[2] Kathete");
+                    out.println("[3] Hypotenuse");
+                    int sideSelector = inputInt("Gib die Seite an:");
+                    out.println("Gib nun die beiden bekannten Seiten an:");
+                    int firstSide = inputInt("[1] Seite:");
+                    int secondSide = inputInt("[2] Seite:");
+                    double calculatedSide = 0;
+                    if (sideSelector == 3) {
+                        calculatedSide = (Math.sqrt((firstSide * firstSide) + (secondSide * secondSide)));
+                    } else {
+                        calculatedSide = (Math.sqrt((firstSide * firstSide) - (secondSide * secondSide)));
+                    }
+                    out.println("Unbekannte Seite: " + calculatedSide);
+                }
+                boolean calculateAgain = inputBoolean("Wollen sie nochmals rechnen? ");
+                rerunProgram = calculateAgain;
+             }while (rerunProgram) ;
+    }
+}
