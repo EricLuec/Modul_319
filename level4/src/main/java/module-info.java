cbd03439fb0da2205b70abde0c19e33c7b0c86e0
ericluec <eric.luecking@gmail.com>
2025-01-28 09:07:53 +0100

    ??

--- level4/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.level4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.level4 to javafx.fxml;
-    exports com.example.level4;
-}
